0d08e505b828024b8ba2319d0cba44d795060f51
gyeongisung <gisung.gyeon@gmail.com>
2023-05-09 11:41:43 +0900

    feat : header 메뉴 기능 1차 적용

---
+++ js/header.js
@@ -0,0 +1,30 @@
+// 웹브라우저에 html,css,js,image 를
+// 모두 블러들여서 렌더링 준비가 끝나면
+// 그때 function의 블럭 안쪽 {}실행!
+window.onload = function () {
+  // header에 마우스 오버하면 header 높이 260px 변경하기
+  // nav에 마우스 아웃하면 header 높이 100px 변경하기
+  // header를 js로 저장해보자(변수 정의해 보자)
+  let header = document.querySelector(".header");
+  // header를 js로 저장해보자(변수 정의해 보자)
+  let nav = document.querySelector(".nav");
+  //   console.log(header);
+  //   console.log(nav);
+
+  // nav에 마우스 오버 처리
+  // html 요소에 마우스 오버 처리하는 법
+  nav.addEventListener("mouseenter", function () {
+    // header의 높이를 260px로 고치고 싶다.
+    // header.style.height = "260px";
+    // 우리는 260px로 변경되는 class를 작성해 두었다.
+    header.classList.add("header-active");
+  });
+  // nav에 마우스 아웃 처리
+  // html 요소에 마우스 아웃 처리하는 법
+  nav.addEventListener("mouseleave", function () {
+    // header의 높이를 100px로 고치고 싶다.
+    // header.style.height = "100px";
+    // 클래스를 제거한다.
+    header.classList.remove("header-active");
+  });
+};
